c0ac00c48faefc3044c0056064995106af3e9e5d
Ulugbek Mirdadayev <ulugbekmirdadayev1211@gmail.com>
2022-09-25 04:33:48 +0500

    update App.js

--- new-insure-x-js/src/App.js
+++ new-insure-x-js/src/App.js
@@ -4,6 +4,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { RootRoutes } from "./routes";
 import { setUser } from "./redux/reducer/user";
 import NotFoundPage from "./components/Ui/NotFound";
+import { setNewMessage } from "./redux/reducer/user";
+import { socket } from "./components/Ui/FooterComponent";
 
 function App() {
   const dispatch = useDispatch();
@@ -31,12 +33,42 @@ function App() {
     return cleanUp();
   }, [user?.id, userStorage?.user?.id]);
 
+  React.useEffect(() => {
+    socket.on("message-send", (msg) => {
+      if (userStorage.role === "sdp") {
+        if (Number(userStorage?.user?.id) === Number(msg?.spec_id)) {
+          dispatch(setNewMessage(true));
+        }
+      }
+      if (userStorage.role === "appraiser") {
+        if (Number(userStorage?.user?.id) === Number(msg?.appraiser_id)) {
+          dispatch(setNewMessage(true));
+        }
+      }
+      if (userStorage.role === "customer") {
+        if (Number(userStorage?.user?.id) === Number(msg?.customer_id)) {
+          dispatch(setNewMessage(true));
+        }
+      }
+      if (userStorage.role === "agent") {
+        if (Number(userStorage?.user?.id) === Number(msg?.agent_id)) {
+          dispatch(setNewMessage(true));
+        }
+      }
+    });
+  }, [userStorage.role, userStorage?.user?.id]);
+
   return (
     <Routes>
       {RootRoutes[userStorage?.role].map(({ path, element }, index) => (
         <Route key={index} path={path} element={element} />
       ))}
-      <Route path={"*"} element={<NotFoundPage role={userStorage?.role} user={userStorage?.user?.id} />} />
+      <Route
+        path={"*"}
+        element={
+          <NotFoundPage role={userStorage?.role} user={userStorage?.user?.id} />
+        }
+      />
     </Routes>
   );
 }
